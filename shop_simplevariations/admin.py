7e81a31d8bd0c9e3e56c4f55de7d1c28f75f37a4
gordon <wgordonw1@gmail.com>
2012-09-10 23:10:38 -0400

    Limit the choices for the OptionGroup "defaults" m2m field to the valid options of the group

--- shop_simplevariations/admin.py
+++ shop_simplevariations/admin.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib import admin
 from django.contrib.admin.options import TabularInline, ModelAdmin
 from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.core.urlresolvers import resolve
 from django.utils.translation import ugettext_lazy as _
 from shop_simplevariations.models import Option, OptionGroup, TextOption
 
@@ -12,12 +13,31 @@ class OptionInline(TabularInline):
 class OptionGroupAdmin(ModelAdmin):
     inlines = [OptionInline,]
     prepopulated_fields = {"slug": ("name",)}
-    formfield_overrides = {
-        models.ManyToManyField: {'widget': FilteredSelectMultiple(
-            verbose_name=_('products'),
-            is_stacked=False
-            )},
-    }
+    
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "products":
+            kwargs.setdefault('widget', FilteredSelectMultiple(
+                    verbose_name=_('products'),
+                    is_stacked=False
+                )
+            )
+        elif db_field.name == "defaults":
+            kwargs.setdefault('widget', FilteredSelectMultiple(
+                    verbose_name=_('default options'),
+                    is_stacked=False
+                )
+            )
+            
+            # limit defaults to valid options of the OptionGroup
+            match = resolve(request.get_full_path())
+            if len(match.args) == 1:
+                object_id = match.args[0]
+            if object_id:
+                og = OptionGroup.objects.get(pk=object_id)
+                option_pks = og.get_options().values_list('pk', flat=True)
+                kwargs.setdefault('queryset', Option.objects.filter(pk__in=option_pks))
+        
+        return super(OptionGroupAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
 admin.site.register(OptionGroup, OptionGroupAdmin)
 
