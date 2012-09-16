d2af965859371038d4727f0c48950db6cc082d12
gordon <wgordonw1@gmail.com>
2012-09-16 10:44:00 -0400

    avoid key error from assuming the first form will have the key 'group' in cleaned_data

--- shop_simplevariations/formsets.py
+++ shop_simplevariations/formsets.py
@@ -6,10 +6,6 @@ class GroupDefaultOptionThroughInlineFormSet(BaseInlineFormSet):
     def clean(self):
         errors = []
         if self.forms:
-            # count choices
-            group = self.forms[0].cleaned_data['group']
-            choose_count = group.get_choose_count()
-            
             # count forms marked for deletion
             delete_forms = [form for form in self.initial_forms if self._should_delete_form(form)]
             deleted_forms_count = len(delete_forms)
@@ -21,16 +17,28 @@ class GroupDefaultOptionThroughInlineFormSet(BaseInlineFormSet):
             # calculate the resultant count after forms are saved
             resultant_forms_count = self.total_form_count() - deleted_forms_count - empty_forms_count
             
-            # compare the resultant count to the group's choice count
-            if choose_count < resultant_forms_count:
-                errors.append(u"Option Group '%(group)s' only has %(choices)s choice%(pc)s.  You specified %(defaults)s default%(pd)s which is %(difference)s too many." % {
-                        "group": group,
-                        "choices": choose_count,
-                        "defaults": resultant_forms_count,
-                        "difference": (resultant_forms_count - choose_count),
-                        "pc": "" if choose_count == 1 else "s",
-                        "pd": "" if resultant_forms_count == 1 else "s"
-                    }
-                )
+            if resultant_forms_count:
+                # find a reference to the group
+                group = None
+                for form in self.forms:
+                    if 'group' in form.cleaned_data:
+                        group = form.cleaned_data['group']
+                        break
+                assert group is not None, "Forms did not reference 'group'"
+                
+                # count choices
+                choose_count = group.get_choose_count()
+                
+                # compare the resultant count to the group's choice count
+                if choose_count < resultant_forms_count:
+                    errors.append(u"Option Group '%(group)s' only has %(choices)s choice%(pc)s.  You specified %(defaults)s default%(pd)s which is %(difference)s too many." % {
+                            "group": group,
+                            "choices": choose_count,
+                            "defaults": resultant_forms_count,
+                            "difference": (resultant_forms_count - choose_count),
+                            "pc": "" if choose_count == 1 else "s",
+                            "pd": "" if resultant_forms_count == 1 else "s"
+                        }
+                    )
         if errors:
             raise ValidationError(errors)
